1dd6cac7ffab3bb410c4538d4668ef81fda28fd5
Junya Ishihara <junya@champierre.com>
2023-03-23 15:05:49 +0900

    日本語ファイル名に対応

--- src/build-readme.js
+++ src/build-readme.js
@@ -53,8 +53,7 @@ class BuildReadme {
 
     readme += "\n\n| データ名 | PDF | 説明 |\n";
     readme += "| --- | --- | --- |\n";
-    readme += "| 小中学校通学区域情報(学校から) | [by_school.pdf](https://raw.githubusercontent.com/takamatsu-city/opendata/main/data/school_area_information/by_school.pdf) | |\n";
-    readme += "| 小中学校通学区域情報(住所から) | [by_address.pdf](https://raw.githubusercontent.com/takamatsu-city/opendata/main/data/school_area_information/by_address.pdf) | |\n";
+    readme += "| 小中学校通学区域情報 | [PDF](https://github.com/takamatsu-city/opendata/tree/main/data/school_area_information) | |\n";
 
     fs.writeFileSync("README.md" , readme);
   }
