9965524b87b40bedadd67283dd21d1ad7bc92889
Junya Ishihara <junya@champierre.com>
2023-03-28 15:56:55 +0900

    説明欄をREADMEから削除

--- src/build-readme.js
+++ src/build-readme.js
@@ -12,8 +12,8 @@ class BuildReadme {
     let readme = "# 高松市オープンデータ\n\n";
     readme += "[サイト(opendata.takamatsu-fact.com/)について（利用規約）](https://opendata.takamatsu-fact.com/about/)\n\n"
     readme += "高松市では、以下のデータをオープンデータとして提供しています。\n\n表内の **CSV** や **GeoJSON** をクリックすると、最新データが得られます。\n\nデータを編集する場合には、**編集**リンクをクリックします。データが地図上に表示され、表組み形式でデータを編集し、編集済みデータをダウンロードすることができます。\n\n位置情報データが誤っている、追加したい、等のご提案には、更新済みのデータの更新をプルリクエストとして送ってください。高松市役所で確認の上、取り込みさせていただきます。詳しい方法は、「[高松市、オープンデータの更新提案の送り方]()」でご案内しています。\n\n";
-    readme += "| データ名 | フォルダ | CSV | GeoJSON | 地図で編集 | 説明 |\n";
-    readme += "| --- | --- | --- | --- | --- | --- |\n";
+    readme += "| データ名 | フォルダ | CSV | GeoJSON | 地図で編集 |\n";
+    readme += "| --- | --- | --- | --- | --- |\n";
     
     for (let i = 0; i < locationDataCategories.length; i++) {
       const category = locationDataCategories[i];
@@ -29,12 +29,12 @@ class BuildReadme {
 
       const filename = path.basename(category.filename, '.xlsx');
 
-      readme += `| ${category.name}(${filename}) | [フォルダ](${csvFolderUrl}) | [CSV](${csvFileUrl}) |[GeoJSON](${jsonFileUrl}) | [編集](${mapUrl}) | ${category.description} |\n`;
+      readme += `| ${category.name}(${filename}) | [フォルダ](${csvFolderUrl}) | [CSV](${csvFileUrl}) |[GeoJSON](${jsonFileUrl}) | [編集](${mapUrl}) |\n`;
     }
 
     readme += "\n以下のデータは位置情報を含まないデータです。\n\n";
-    readme += "| データ名 | フォルダ | CSV | JSON | 説明 |\n";
-    readme += "| --- | --- | --- | --- | --- |\n";
+    readme += "| データ名 | フォルダ | CSV | JSON |\n";
+    readme += "| --- | --- | --- | --- |\n";
 
     for (let i = 0; i < standardDataCategories.length; i++) {
       const category = standardDataCategories[i];
@@ -49,22 +49,22 @@ class BuildReadme {
           const csvFileUrl = `https://opendata.takamatsu-fact.com/${category.category}/${filename}.csv`;          
           const subCategory = filename.split('_')[1];
           if (file === csvFiles[0]) {
-            readme += `| ${category.name}(${category.filename}) | [フォルダ](${csvFolderUrl}) | [CSV](${csvFileUrl}) | [JSON(${subCategory})](${jsonFileUrl}) | ${category.description} |\n`;
+            readme += `| ${category.name}(${category.filename}) | [フォルダ](${csvFolderUrl}) | [CSV](${csvFileUrl}) | [JSON(${subCategory})](${jsonFileUrl}) |\n`;
           } else {
-            readme += `||| [CSV](${csvFileUrl}) | [JSON(${subCategory})](${jsonFileUrl}) | ${category.description} |\n`;
+            readme += `||| [CSV](${csvFileUrl}) | [JSON(${subCategory})](${jsonFileUrl}) |\n`;
           } 
         });
       } else if (category.historical) {
-        readme += `| ${category.name}(${category.filename}) | [フォルダ](${csvFolderUrl}) | [CSV(最新データ)](${csvFileUrl}) | [JSON(最新データ)](${jsonFileUrl}) | ${category.description} |\n`;
+        readme += `| ${category.name}(${category.filename}) | [フォルダ](${csvFolderUrl}) | [CSV(最新データ)](${csvFileUrl}) | [JSON(最新データ)](${jsonFileUrl}) |\n`;
       } else {
-        readme += `| ${category.name}(${category.filename}) | [フォルダ](${csvFolderUrl}) | [CSV](${csvFileUrl}) | [JSON](${jsonFileUrl}) | ${category.description} |\n`;  
+        readme += `| ${category.name}(${category.filename}) | [フォルダ](${csvFolderUrl}) | [CSV](${csvFileUrl}) | [JSON](${jsonFileUrl}) |\n`;  
       }
     }
 
-    readme += "\n\n| データ名 | PDF | 説明 |\n";
-    readme += "| --- | --- | --- |\n";
-    readme += "| 小中学校通学区域情報 | [PDF](https://github.com/takamatsu-city/opendata/tree/main/data/school_area_information) | |\n";
-    readme += "| 令和2年度市民満足度アンケート結果 | [PDF](https://github.com/takamatsu-city/opendata/tree/main/data/citizen_satisfaction_questionnaire_result_r2) | |\n";
+    readme += "\n\n| データ名 | PDF |\n";
+    readme += "| --- | --- |\n";
+    readme += "| 小中学校通学区域情報 | [PDF](https://github.com/takamatsu-city/opendata/tree/main/data/school_area_information) |\n";
+    readme += "| 令和2年度市民満足度アンケート結果 | [PDF](https://github.com/takamatsu-city/opendata/tree/main/data/citizen_satisfaction_questionnaire_result_r2) |\n";
 
     fs.writeFileSync("README.md" , readme);
   }
