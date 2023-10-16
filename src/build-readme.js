d9219ed6e863e301d9df0dae74d980db484220c5
naoki <n.globe.us@gmail.com>
2023-10-16 11:51:37 +0900

    some fix

--- src/build-readme.js
+++ src/build-readme.js
@@ -17,9 +17,9 @@ class BuildReadme {
     
     for (let i = 0; i < locationDataCategories.length; i++) {
       const category = locationDataCategories[i];
-      
+
       const csvFile = glob.sync(`data/${category.category}/*.csv`)[0];
-      
+
       const csvFolderUrl = `https://github.com/takamatsu-city/opendata/tree/main/data/${category.category}`;
 
       const csvFileUrl = `https://opendata.takamatsu-fact.com/${category.category}/data.csv`;
